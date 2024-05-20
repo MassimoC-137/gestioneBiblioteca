1e512c03cba72dca6de1afb7d7840a4f4dac78f0
MassimoC-137 <massimo.caramanna@gmail.com>
2024-05-20 12:29:35 +0200

    primo commit

---
+++ src/main/java/it/prova/gestionebiblioteca/GestionebibliotecaApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionebiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionebibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionebibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/gestionebiblioteca/GestionebibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionebiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionebibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
